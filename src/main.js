751c9d70bda2c01443d6d90192b023c9759c56d4
Andres Benitez <spartansup18@gmail.com>
2022-04-11 11:48:18 -0700

    added borders and scrolling starfield

--- src/main.js
+++ src/main.js
@@ -4,4 +4,9 @@ let config = {
     height: 480,
     scene: [ Menu, Play ]
 }
+
 let game = new Phaser.Game(config);
+
+// set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
